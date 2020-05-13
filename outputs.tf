2f2ed65c7c5f5397a518dab8157b6660ad86b7aa
Romain Barissat <romain-noreply@barissat.com>
2020-05-13 14:41:32 +0700

    Clean up

--- outputs.tf
+++ outputs.tf
@@ -34,31 +34,3 @@ output "cognito_domain_alias" {
 output "aws_signin_url" {
   value = "https://${var.auth0_domain}/samlp/${auth0_client.aws.id}?connection=${module.cognito_users_label.id}&RelayState=https://console.aws.amazon.com/console/home?region=${local.aws_default_region}"
 }
-
-/*
-output "auth0_create_conn" {
-  value       = <<EOF
-  {
-    "options": {
-      "type": "back_channel",
-      "scope": "openid profile email",
-      "client_id": "${aws_cognito_user_pool_client.auth0.id}",
-      "client_secret": "${aws_cognito_user_pool_client.auth0.client_secret}",
-      "issuer": "https://${aws_cognito_user_pool.users.endpoint}",
-      "jwks_uri": "https://${aws_cognito_user_pool.users.endpoint}/.well-known/jwks.json",
-      "discovery_url": "https://${aws_cognito_user_pool.users.endpoint}/.well-known/openid-configuration",
-      "token_endpoint": "${local.aws_cognito_auth_url}/oauth2/token",
-      "userinfo_endpoint": "${local.aws_cognito_auth_url}/oauth2/userInfo",
-      "authorization_endpoint": "${local.aws_cognito_auth_url}/oauth2/authorize"
-    },
-    "strategy": "oidc",
-    "name": "${module.cognito_users_label.id}",
-    "is_domain_connection": false,
-    "enabled_clients": [
-      "${auth0_client.aws.id}"
-    ]
-  }
-EOF
-  description = "Create Auth0 connection"
-}
-*/
